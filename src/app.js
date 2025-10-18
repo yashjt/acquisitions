64a930c4ceb4537c1f2625619563b89e2efe5cc9
yashjt <yashjt080@gmail.com>
2025-10-18 00:55:46 -0400

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from Acquisition");
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from Acquisition');
 });
 
 export default app;
